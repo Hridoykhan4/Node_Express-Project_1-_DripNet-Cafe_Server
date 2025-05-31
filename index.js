f1c35c4175c78073b71609731dbcba0ef8ec2036
Hridoykhan4 <hridoykhan148385@gmail.com>
2025-05-31 15:07:57 +0600

    testing

--- index.js
+++ index.js
@@ -6,13 +6,18 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
+/* 
+https://i.ibb.co/N6Jxhrk0/1.png
+
+*/
+
 // middleware
 app.use(cors());
 app.use(express.json());
 
 // MongoDB Section
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.n0qsrr5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-
+// const uri = `mongodb://localhost:27017`
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -27,16 +32,40 @@ async function run() {
     const coffeeCollection = client.db("coffeeDB").collection("coffee");
 
     // Get All Coffees
-    app.get('/coffee', async(req, res) => {
-        const cursor = coffeeCollection.find();
-        const result = await cursor.toArray();
-        res.send(result)
-    })
+    app.get("/coffee", async (req, res) => {
+      const cursor = coffeeCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
     //  Get Specific Coffee
-    
+    app.get("/coffee/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await coffeeCollection.findOne(query);
+      res.send(result);
+    });
 
-
+    // PUT A Coffee
+    app.put("/coffee/:id", async (req, res) => {
+      const updatedCoffee = req.body;
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const coffee = {
+        $set: {
+          name: updatedCoffee.name,
+          quantity: updatedCoffee.quantity,
+          supplier: updatedCoffee.supplier,
+          taste: updatedCoffee.taste,
+          category: updatedCoffee.category,
+          details: updatedCoffee.details,
+          photo: updatedCoffee.photo,
+        },
+      };
+      const result = await coffeeCollection.updateOne(filter, coffee, options);
+      res.send(result);
+    });
 
     // POST A Coffee
     app.post("/coffee", async (req, res) => {
@@ -45,14 +74,13 @@ async function run() {
       res.send(result);
     });
 
-
     // Delete A Coffee
-    app.delete('/coffee/:coffeeId', async(req, res) => {
-        const id = req.params.coffeeId;
-        const query = {_id: new ObjectId(id)};
-        const result = await coffeeCollection.deleteOne(query);
-        res.send(result);
-    })
+    app.delete("/coffee/:coffeeId", async (req, res) => {
+      const id = req.params.coffeeId;
+      const query = { _id: new ObjectId(id) };
+      const result = await coffeeCollection.deleteOne(query);
+      res.send(result);
+    });
 
     await client.db("admin").command({ ping: 1 });
     console.log(
